9f02bd342d23dec2f35343c8ee6a3cd645ed50cd
Gabor Soos <gabor.soos@byborgenterprises.com>
2024-09-16 16:35:25 +0200

    add list and add endpoint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 const fastify = require('fastify')({ logger: true });
 
+const videos = [{ id: 0, name: 'Dummy Video', payload: '{}' }];
+
 // Declare a route
 fastify.get('/', function handler (request, reply) {
-    reply.send({ hello: 'world' });
+  reply.send({ hello: 'world' });
+});
+
+fastify.get('/videos', function handler (request, reply) {
+  reply.send(videos);
+});
+
+fastify.post('/videos', function handler (request, reply) {
+  const video = { id: videos.length, name: request.body.name || 'undefined', payload: JSON.stringify(request.body) };
+  videos.push(video);
+
+  reply.send(video);
 });
 
 const port = process.env.PORT || 10000;
@@ -10,8 +23,8 @@ const host = process.env.HOST || 'localhost';
 
 // Run the server!
 fastify.listen({ host, port }, (err) => {
-    if (err) {
-        fastify.log.error(err)
-        process.exit(1)
-    }
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
 });
