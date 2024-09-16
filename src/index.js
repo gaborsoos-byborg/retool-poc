1e5e2fbb6150648f54e9bf6a2f23f5e6f0af290c
Gabor Soos <gabor.soos@byborgenterprises.com>
2024-09-16 16:12:21 +0200

    extract port

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ fastify.get('/', function handler (request, reply) {
     reply.send({ hello: 'world' });
 });
 
+const port = process.env.PORT || 10000;
+
 // Run the server!
-fastify.listen({ port: process.env.PORT || 3000 }, (err) => {
+fastify.listen({ port }, (err) => {
     if (err) {
         fastify.log.error(err)
         process.exit(1)
